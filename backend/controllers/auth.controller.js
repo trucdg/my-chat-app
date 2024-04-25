1938cb6ebe0dd28b76fd53f302557894d2d2fb22
Truc Duong <trucdg@bu.edu>
2024-04-25 12:35:11 -0400

    write simple controllers for signup, login, logout routes

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,11 @@
+export const signup = (req, res) => {
+    console.log("sign up!!")
+}
+
+export const login = (req, res) => {
+    console.log("login User!");
+}
+
+export const logout = (req, res) => {
+    console.log("logout User!");
+}
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
 
+import authRoutes from "./routes/auth.routes.js";
+
 // create an express server
 const app = express();
 
@@ -12,16 +14,6 @@ app.get("/", (req, res) => {
     res.send("Server is ready now!!");
 })
 
-app.get("/api/auth/signup", (req, res) => {
-    console.log("signup route");
-})
-
-app.get("/api/auth/login", (req, res) => {
-    console.log("login route");
-})
-
-app.get("/api/auth/logout", (req, res) => {
-    console.log("logout route");
-})
+app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
