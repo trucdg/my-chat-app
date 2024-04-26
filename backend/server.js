fc6659d49c86a0c003910bdcf95e7d077c658ace
Truc Duong <trucdg@bu.edu>
2024-04-26 11:40:24 -0400

    create and handle user requests to signup endpoint

--- backend/server.js
+++ backend/server.js
@@ -2,18 +2,26 @@ import express from "express";
 import dotenv from "dotenv";
 
 import authRoutes from "./routes/auth.routes.js";
+import connectToMongoDB from "./db/connectToMongoDB.js";
 
 // create an express server
 const app = express();
-
 dotenv.config();
+
+// variables
 const PORT = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-    //root route: http://localhost:3000/
-    res.send("Server is ready now!!");
-})
-
+//middleware
+app.use(express.json()); //parse the incoming requests with json payloads (from req.body)
 app.use("/api/auth", authRoutes);
 
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+// app.get("/", (req, res) => {
+//     //root route: http://localhost:3000/
+//     res.send("Server is ready now!!");
+// })
+
+
+app.listen(PORT, () => {
+    connectToMongoDB();
+    console.log(`Server is running on ${PORT}`);
+});
