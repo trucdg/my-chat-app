b8d31a2548ef8b3ba525413c9d220f8d986dcd07
Truc Duong <trucdg@bu.edu>
2024-04-29 12:00:48 -0400

    add Conversation and Conversations components

--- frontend/src/components/sidebar/Sidebar.jsx
+++ frontend/src/components/sidebar/Sidebar.jsx
@@ -1,4 +1,4 @@
-// import Conversations from "./Conversations";
+import Conversations from "./Conversations";
 // import LogoutButton from "./LogoutButton";
 import SearchInput from "./SearchInput";
 
@@ -7,8 +7,8 @@ const Sidebar = () => {
 		<div className='border-r border-slate-500 p-4 flex flex-col'>
 			<SearchInput />
 			<div className='divider px-3'></div>
-			{/* <Conversations />
-			<LogoutButton /> */}
+			<Conversations />
+			{/* <LogoutButton /> */}
 		</div>
 	);
 };
