f73ee3e779aacb47dd27a1f3eedf1f28f5531789
Truc Duong <trucdg@bu.edu>
2024-04-29 09:59:07 -0400

    add sendMessage endpoint and add middleware to protect route

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js"
 import connectToMongoDB from "./db/connectToMongoDB.js";
 
 // create an express server
@@ -13,10 +15,12 @@ const PORT = process.env.PORT || 3000;
 
 //middleware
 app.use(express.json()); //parse the incoming requests with json payloads (from req.body)
+app.use(cookieParser());
 app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 
 // app.get("/", (req, res) => {
-//     //root route: http://localhost:3000/
+//     //root route: http://localhost:3000/ 
 //     res.send("Server is ready now!!");
 // })
 
