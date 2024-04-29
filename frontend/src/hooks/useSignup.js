f4270578f374dd0247fc683c5bc5c12af24e1dfa
Truc Duong <trucdg@bu.edu>
2024-04-29 13:30:57 -0400

    create AuthContext

--- frontend/src/hooks/useSignup.js
+++ frontend/src/hooks/useSignup.js
@@ -1,10 +1,10 @@
 import { useState } from "react";
 import toast from "react-hot-toast";
-//import { useAuthContext } from "../context/AuthContext";
+import { useAuthContext } from "../context/AuthContext";
 
 const useSignup = () => {
 	const [loading, setLoading] = useState(false);
-	//const { setAuthUser } = useAuthContext();
+	const { setAuthUser } = useAuthContext();
 
 	const signup = async ({ fullName, username, password, confirmPassword, gender }) => {
 		const success = handleInputErrors({ fullName, username, password, confirmPassword, gender });
@@ -22,8 +22,8 @@ const useSignup = () => {
 			if (data.error) {
 				throw new Error(data.error);
 			}
-			//localStorage.setItem("chat-user", JSON.stringify(data));
-			//setAuthUser(data);
+			localStorage.setItem("chat-user", JSON.stringify(data));
+			setAuthUser(data);
 
 		} catch (error) {
 			toast.error(error.message);
